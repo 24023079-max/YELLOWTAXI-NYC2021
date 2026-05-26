a2742fb356f85432a68cda6f22529b53ec22bf30
24023079-max <24023079@vnu.edu.vn>
2026-05-27 00:54:01 +0700

    làm sạch
    làm sạch

--- Data/src/dowload_dataraw.py
+++ Data/src/dowload_dataraw.py
@@ -1,18 +1,29 @@
 import os
 import requests
 
-months = range(1, 13)
 os.makedirs("Data/raw", exist_ok=True)
+os.makedirs("Data/processed", exist_ok=True)
 
-for m in months:
+# tải dữ liệu theo tháng
+for m in range(1, 13):
     month = f"{m:02d}"
     url = f"https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_2021-{month}.parquet"
-    filename = f"yellow_tripdata_2021-{month}.parquet"
+    path = f"Data/raw/yellow_tripdata_2021-{month}.parquet"
 
-    print("Downloading:", filename)
-    r = requests.get(url)
+    if not os.path.exists(path):
+        print("Downloading:", month)
+        r = requests.get(url)
+        with open(path, "wb") as f:
+            f.write(r.content)
 
-    with open(f"Data/raw/{filename}", "wb") as f:
+# tải thêm file taxi zone lookup
+lookup_url = "https://d37ci6vzurychx.cloudfront.net/misc/taxi_zone_lookup.csv"
+lookup_path = "Data/raw/taxi_zone_lookup.csv"
+
+if not os.path.exists(lookup_path):
+    print("Downloading: taxi_zone_lookup.csv")
+    r = requests.get(lookup_url)
+    with open(lookup_path, "wb") as f:
         f.write(r.content)
 
-print("DONE!")
+print("DONE")
